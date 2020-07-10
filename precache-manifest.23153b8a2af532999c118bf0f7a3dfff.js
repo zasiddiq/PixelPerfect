c947e409222a298dd66acd6e22103b187f6a5977
tt081866 <tristan.tyler@cerner.com>
2020-07-10 01:04:34 -0500

    Updates

---
+++ precache-manifest.23153b8a2af532999c118bf0f7a3dfff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8016469371a21af24ef2efaaea27e92",
+    "url": "/PixelPerfect/index.html"
+  },
+  {
+    "revision": "56a78af8d5389e8e4d7d",
+    "url": "/PixelPerfect/static/css/main.61d9f342.chunk.css"
+  },
+  {
+    "revision": "82bc0d80d59b9d0b5bab",
+    "url": "/PixelPerfect/static/js/2.efacc8d5.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/PixelPerfect/static/js/2.efacc8d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a78af8d5389e8e4d7d",
+    "url": "/PixelPerfect/static/js/main.f94e7412.chunk.js"
+  },
+  {
+    "revision": "443c22947c4931546e35",
+    "url": "/PixelPerfect/static/js/runtime-main.b791c6eb.js"
+  }
+]);
